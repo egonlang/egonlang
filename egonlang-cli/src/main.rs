7010e00fede972a13a6d354aa2fc23117d925bbf
Kylee Tilley <kyleetilley@gmail.com>
2024-04-21 21:43:32 -0500

    Add clap to cli

--- egonlang-cli/src/main.rs
+++ egonlang-cli/src/main.rs
@@ -1,63 +1,88 @@
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
 use egonlang_core::validator::Validator;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Parse file in to an serialized AST
+    Parse {
+        /// Path to file
+        path: PathBuf,
+
+        /// Input file is a serialized tokens file
+        #[arg(short, long)]
+        use_tokens_file: bool,
+    },
+    /// Lex file in to an serialized array of tokens
+    Lex {
+        /// Path to file
+        path: PathBuf,
+    },
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = args.get(2).expect("No command passed: lex, parse");
+    let cli = Cli::parse();
 
-    match command.as_str() {
-        "parse" => {
-            let path = args.get(3).expect("No path argument passed");
-            let pathbuf = PathBuf::from(path);
-            let content = std::fs::read_to_string(path).expect("Unable to read file");
-            let path = std::fs::canonicalize(pathbuf).unwrap();
+    match &cli.command {
+        Some(command) => match command {
+            Commands::Parse {
+                path,
+                use_tokens_file: _,
+            } => {
+                let content = std::fs::read_to_string(&path).expect("Unable to read file");
+                let path = std::fs::canonicalize(&path).unwrap();
 
-            let module = match egonlang_core::parser::parse(&content, 0) {
-                Ok(module) => {
-                    if let Err(errs) = Validator::default().validate(&module) {
-                        Err(errs)
-                    } else {
-                        Ok(serde_json::to_string(&module).unwrap())
+                let module = match egonlang_core::parser::parse(&content, 0) {
+                    Ok(module) => {
+                        if let Err(errs) = Validator::default().validate(&module) {
+                            Err(errs)
+                        } else {
+                            Ok(serde_json::to_string(&module).unwrap())
+                        }
                     }
-                }
-                Err(errs) => Err(errs),
-            };
+                    Err(errs) => Err(errs),
+                };
 
-            match module {
-                Ok(module) => {
-                    println!("{module}");
-                }
-                Err(errs) => {
-                    println!("Path:\n\n{:?}\n", &path);
-                    println!("Input:\n\n{content}\n");
-                    println!("Errors:\n\n{errs:#?}");
-                }
-            };
-        }
-        "lex" => {
-            let path = args.get(3).expect("No path argument passed");
-            let pathbuf = PathBuf::from(path);
-            let content = std::fs::read_to_string(path).expect("Unable to read file");
-            let path = std::fs::canonicalize(pathbuf).unwrap();
+                match module {
+                    Ok(module) => {
+                        println!("{module}");
+                    }
+                    Err(errs) => {
+                        println!("Path:\n\n{:?}\n", &path);
+                        println!("Input:\n\n{content}\n");
+                        println!("Errors:\n\n{errs:#?}");
+                    }
+                };
+            }
+            Commands::Lex { path } => {
+                let content = std::fs::read_to_string(&path).expect("Unable to read file");
+                let path = std::fs::canonicalize(&path).unwrap();
 
-            let tokens = egonlang_core::lexer::Lexer::new(&content).collect::<Vec<_>>();
+                let tokens = egonlang_core::lexer::Lexer::new(&content).collect::<Vec<_>>();
 
-            match serde_json::to_string(&tokens) {
-                Ok(tokens) => {
-                    println!("{tokens}");
-                }
-                Err(errs) => {
-                    println!("Path:\n\n{path:?}\n",);
-                    println!("Input:\n\n{content}\n");
-                    println!("Errors:\n\n{errs}");
-                }
-            };
-        }
-        _ => {
-            println!(
-                "Invalid command! Expected one of [\"lex\", \"parse\"] but received \"{command}\""
-            );
+                match serde_json::to_string(&tokens) {
+                    Ok(tokens) => {
+                        println!("{tokens}");
+                    }
+                    Err(errs) => {
+                        println!("Path:\n\n{path:?}\n",);
+                        println!("Input:\n\n{content}\n");
+                        println!("Errors:\n\n{errs}");
+                    }
+                };
+            }
+        },
+        None => {
+            println!("Invalid command! Expected one of [\"lex\", \"parse\"] ");
         }
     }
 }
