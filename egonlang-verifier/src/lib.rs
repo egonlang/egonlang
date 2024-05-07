950676b4afcb9fce9298000c92329fe1c53cf3f6
Kylee Tilley <kyleetilley@gmail.com>
2024-05-07 16:44:35 -0500

    Refactor

--- egonlang-verifier/src/lib.rs
+++ egonlang-verifier/src/lib.rs
@@ -7,10 +7,12 @@ pub mod visitor;
 use egonlang_core::{ast::Module, parser::parse};
 use verifier::{VerificationResult, Verifier};
 
+/// Parse source string and verify the module AST
 pub fn verify_source(source: &str) -> VerificationResult {
     parse(source, 0).and_then(|module| verify_module(&module))
 }
 
+/// Verify the module AST
 pub fn verify_module(module: &Module) -> VerificationResult {
     Verifier::new().verify(module)
 }
