02a6e914703072516e448d0c2d59c4908c377cf6
Kylee Tilley <kyleetilley@gmail.com>
2024-07-22 19:31:25 -0500

    Add another cli test

--- egonlang-cli/src/main.rs
+++ egonlang-cli/src/main.rs
@@ -40,10 +40,9 @@ fn main() {
     match &cli.command {
         Some(command) => match command {
             Commands::Verify { path } => {
-                println!("Path:\n\n{:?}\n", &path);
-                let path = std::fs::canonicalize(path).unwrap();
+                let full_path = std::fs::canonicalize(path).unwrap();
 
-                let content = std::fs::read_to_string(&path).expect("Unable to read file");
+                let content = std::fs::read_to_string(full_path).expect("Unable to read file");
 
                 let module = match parse(&content, 0) {
                     Ok(module) => {
@@ -58,7 +57,6 @@ fn main() {
                         println!("Passed!");
                     }
                     Err(errs) => {
-                        println!("Path:\n\n{:?}\n", &path);
                         println!("Input:\n\n{content}\n");
                         println!("Errors:\n\n{errs:#?}");
                     }
@@ -68,8 +66,8 @@ fn main() {
                 path,
                 use_tokens_file: _,
             } => {
-                let content = std::fs::read_to_string(path).expect("Unable to read file");
                 let path = std::fs::canonicalize(path).unwrap();
+                let content = std::fs::read_to_string(path).expect("Unable to read file");
 
                 let module = match egonlang_core::parser::parse(&content, 0) {
                     Ok(module) => Ok(serde_json::to_string(&module).unwrap()),
@@ -81,15 +79,14 @@ fn main() {
                         println!("{module}");
                     }
                     Err(errs) => {
-                        println!("Path:\n\n{:?}\n", &path);
                         println!("Input:\n\n{content}\n");
                         println!("Errors:\n\n{errs:#?}");
                     }
                 };
             }
             Commands::Lex { path } => {
-                let content = std::fs::read_to_string(path).expect("Unable to read file");
                 let path = std::fs::canonicalize(path).unwrap();
+                let content = std::fs::read_to_string(path).expect("Unable to read file");
 
                 let tokens = egonlang_core::lexer::Lexer::new(&content).collect::<Vec<_>>();
 
@@ -98,7 +95,6 @@ fn main() {
                         println!("{tokens}");
                     }
                     Err(errs) => {
-                        println!("Path:\n\n{path:?}\n",);
                         println!("Input:\n\n{content}\n");
                         println!("Errors:\n\n{errs}");
                     }
@@ -121,12 +117,66 @@ mod tests {
     fn can_verify_a_valid_file() -> Result<(), Box<dyn std::error::Error>> {
         let mut cmd = Command::cargo_bin("egon")?;
 
-        cmd.current_dir(std::env::current_dir()?);
-
-        cmd.arg("verify").arg("../res/examples/valid/assign.eg");
-        cmd.assert()
+        cmd.arg("verify")
+            .arg("../res/examples/valid/assign.eg")
+            .assert()
             .success()
             .stdout(predicate::eq(b"Passed!\n" as &[u8]));
+
+        Ok(())
+    }
+
+    #[test]
+    fn can_verify_an_invalid_file() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("egon")?;
+
+        cmd.arg("verify")
+            .arg("../res/examples/invalid/divide_by_zero.eg")
+            .assert()
+            .success()
+            .stdout(predicate::eq(
+                r##"Input:
+
+0 / 1;
+1 / 0;
+0 / 0;
+
+// out: SyntaxError: can not divide by zero
+// out: SyntaxError: can not divide by zero
+// out: SyntaxError: can not divide by zero
+// out: SyntaxError: can not divide by zero
+
+Errors:
+
+[
+    (
+        SyntaxError(
+            DivideByZero,
+        ),
+        0..1,
+    ),
+    (
+        SyntaxError(
+            DivideByZero,
+        ),
+        11..12,
+    ),
+    (
+        SyntaxError(
+            DivideByZero,
+        ),
+        14..15,
+    ),
+    (
+        SyntaxError(
+            DivideByZero,
+        ),
+        18..19,
+    ),
+]
+"##
+                .to_string(),
+            ));
 
         Ok(())
     }
