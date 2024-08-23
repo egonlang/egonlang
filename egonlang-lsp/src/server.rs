067036d7bc00abf59d5e53f1d6ea5b3a12e5ee41
Kylee Tilley <kyleetilley@gmail.com>
2024-08-23 02:54:11 -0500

    Refactor

--- egonlang-lsp/src/server.rs
+++ egonlang-lsp/src/server.rs
@@ -6,7 +6,7 @@ use egonlang_core::prelude::*;
 use egonlang_verifier::prelude::*;
 use tokio::sync::Mutex;
 use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::{HoverProviderCapability, MessageType, Url};
+use tower_lsp::lsp_types::{HoverProviderCapability, Url};
 use tower_lsp::{
     jsonrpc,
     lsp_types::{
